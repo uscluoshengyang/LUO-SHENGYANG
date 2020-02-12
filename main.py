77e83bc7740d34770fe81cb6b29832a38ec586df
Bill Risher <rishe007@umn.edu>
2020-02-12 15:50:28 +0000

    added python files

---
+++ main.py
@@ -0,0 +1,12 @@
+# Encephalo Investments Coding Pre-Test - main.py
+import pandas as pd
+
+
+def main():
+    # main function.
+    # Return the top 10 companies with the highest market cap.
+    data = pd.read_csv("./data.csv") # read csv using pandas
+
+
+if __name__ == "__main__":
+    main()
